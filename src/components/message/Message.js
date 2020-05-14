3fa152e89b00753403db9d72178399ca0613ed25
Martijn Nieuwenhuizen <mnieuwenhuizen@mirabeau.nl>
2020-05-14 08:46:32 +0200

    make messages random

--- src/components/message/Message.js
+++ src/components/message/Message.js
@@ -1,9 +1,14 @@
+import messages from '../../data/messages'
+import randomNumber from '../../static/js/utils/randomNumber'
+
 export default class Message {
   constructor(element, options) {
     this._element = element
     this._options = options
 
     this._handleClick = this._handleClick.bind(this)
+
+    this._messages = messages.sort(() => Math.random() - 0.5)
 
     this._body = document.querySelector('body')
     this._text = document.querySelector('.message__text')
@@ -13,11 +18,13 @@ export default class Message {
   }
 
   _setText() {
-    const text = 'More to come!'
-    this._text.innerHTML = text
+    const randomMessageId = randomNumber(1, this._messages.length - 1)
+    this._text.innerHTML = this._messages[randomMessageId]
   }
 
   _handleClick() {
+    this._setText()
+
     this._body.setAttribute('data-show-message', 'false')
     this._body.setAttribute('data-drops', 'false')
   }
